3af913cf1ebe8885929e50dc090a1965ec508823
Nicholas Iasso <nicholasi@shift.com>
2020-07-10 22:43:18 -0400

    Added checkpoints and reset feature

--- godotproject/character/Character.gd
+++ godotproject/character/Character.gd
@@ -7,6 +7,8 @@ export(float) var THRUST_FORCE: float = 200.0
 export(float) var INPUT_DELAY_MSECS: int = 1000
 
 var input_slices: Array = []
+var active_checkpoint: Checkpoint
+var colliding_checkpoint: Checkpoint
 
 class InputSlice:
 	# I'm not using getters and setters here because
@@ -18,9 +20,20 @@ class InputSlice:
 	
 func _process(_delta: float) -> void:
 	save_current_input()
+	print(rotation)
+	print(linear_velocity)
+	if !!colliding_checkpoint:
+		if rotation < 0.1 and linear_velocity.length_squared() <= 0.1:
+			active_checkpoint = colliding_checkpoint
 
-func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
-
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	if Input.is_action_just_pressed("reset"):
+		state.transform = Transform2D(0.0, active_checkpoint.global_position)
+		state.linear_velocity = Vector2.ZERO
+		state.angular_velocity = 0.0
+		input_slices = []
+		return
+		
 	var current_input: InputSlice = pop_and_return_delayed_input()
 	if !current_input:
 		return
@@ -47,3 +60,13 @@ func pop_and_return_delayed_input() -> InputSlice:
 	while input_slices.size() != 0 and input_slices.front().game_tick_msec + INPUT_DELAY_MSECS < current_game_tick_msec:
 		result = input_slices.pop_front()
 	return result
+
+func _on_CheckpointCollider_area_entered(area: Checkpoint):
+	if !area:
+		return
+	colliding_checkpoint = area
+
+func _on_CheckpointCollider_area_exited(area: Checkpoint):
+	if !area:
+		return
+	colliding_checkpoint = null
