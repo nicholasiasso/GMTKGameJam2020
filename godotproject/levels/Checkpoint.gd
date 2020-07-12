c04a8c8c51150369624792a51a328628e413f12e
Nicholas Iasso <nicholasi@shift.com>
2020-07-11 23:39:38 -0400

    removed checkpoints when the deactivate and dont display active flags

--- godotproject/levels/Checkpoint.gd
+++ godotproject/levels/Checkpoint.gd
@@ -3,6 +3,7 @@ extends Area2D
 onready var hover_animation_player: AnimationPlayer = $HoverAnimationPlayer
 onready var glow_animation_player: AnimationPlayer = $ActivatedGlow/GlowAnimationPlayer
 onready var activated_glow: Sprite = $ActivatedGlow
+onready var sprite: Sprite = $Sprite
 
 onready var camera_center: Node2D = $CameraCenter
 var camera_position: Vector2
@@ -19,8 +20,12 @@ func _ready() -> void:
 	
 	
 func set_is_active(active: bool) -> void:
-	is_active = active
-	activated_glow.visible = active
+	if active:
+		sprite.visible = false
+		activated_glow.visible = active
+		is_active = active
+	else:
+		queue_free()
 
 func get_spawn_position() -> Vector2:
 	return global_position + Vector2.UP * 6
