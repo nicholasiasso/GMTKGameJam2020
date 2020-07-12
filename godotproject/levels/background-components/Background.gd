c3fd611a47382347476650301e3c3da17e448db8
bestjayd <jamesond@vt.edu>
2020-07-12 13:34:26 -0400

    Background is in entire level

--- godotproject/levels/background-components/Background.gd
+++ godotproject/levels/background-components/Background.gd
@@ -4,18 +4,19 @@ export var initial_dust_count: int = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(initial_dust_count):
-		add_random_dust(rand_range(0, ProjectSettings.get_setting("display/window/size/width")))
+#	for i in range(initial_dust_count):
+#		add_random_dust(rand_range(0, ProjectSettings.get_setting("display/window/size/width")))
 		
 	pass # Replace with function body.
 
-func add_random_dust(x_offset: int =0) -> void:
-	$DustSpawnPath/DustSpawner.offset = randi()
+func add_random_dust(position: Vector2) -> void:
 	var dust = load("res://levels/background-components/Dust.tscn").instance()
 	add_child(dust)
-	dust.position = $DustSpawnPath/DustSpawner.position
-	dust.position = Vector2(dust.position.x - x_offset, dust.position.y)
+	dust.position = position
 
+func _on_DustSpawnPath_add_dust(position):
+	position += $DustSpawnPath.position
+	add_random_dust(position)
 
-func _on_DustTimer_timeout():
-	add_random_dust()
+func move_spawner(position):
+	$DustSpawnPath.position += position
